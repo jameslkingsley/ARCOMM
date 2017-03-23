1f9fbff0b40296e76229c28634facc096ebb1425
James Kingsley <jameslkingsley@outlook.com>
2017-03-23 02:47:18 +0000

    Minify

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -4,10 +4,10 @@
  * code may be modified to fit the specific needs of your application.
  */
 
-window._ = require('lodash');
+// window._ = require('lodash');
 window.$ = window.jQuery = require('jquery');
-window.Vue = require('vue');
-window.axios = require('axios');
+// window.Vue = require('vue');
+// window.axios = require('axios');
 
 require('jquery-ui-bundle');
 require('bootstrap-sass');
