85f94b9aab9dd2d87a106f3f77c6cdd233504e52
James Kingsley <jameslkingsley@outlook.com>
2017-03-27 12:00:50 +0100

    Add key file path

--- config/filesystems.php
+++ config/filesystems.php
@@ -78,7 +78,7 @@ return [
         'gcs' => [
             'driver' => 'gcs',
             'project_id' => env('GOOGLE_CLOUD_PROJECT_ID', 'your-project-id'),
-            'key_file' => env('GOOGLE_CLOUD_KEY_FILE', null),
+            'key_file' => base_path('gcs.json'),
             'bucket' => env('GOOGLE_CLOUD_STORAGE_BUCKET', 'your-bucket'),
             'path_prefix' => env('GOOGLE_CLOUD_STORAGE_PATH_PREFIX', null),
             'storage_api_uri' => env('GOOGLE_CLOUD_STORAGE_API_URI', null),
