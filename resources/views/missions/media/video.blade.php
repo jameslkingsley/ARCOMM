8789936da4612127dcab18ce260f87a064896a4b
James Kingsley <jameslkingsley@outlook.com>
2017-05-02 16:14:48 +0100

    Fix videos

--- resources/views/missions/media/video.blade.php
+++ resources/views/missions/media/video.blade.php
@@ -2,7 +2,7 @@
     href="{{ $video->url() }}"
     target="_newtab"
     class="mission-media-item mission-media-item-video"
-    style="background-image: url({{ $video->info()->snippet->thumbnails->standard->url }})">
+    style="background-image: url({{ $video->info()->snippet->thumbnails->high->url }})">
 
     @if ($video->isMine() || auth()->user()->hasPermission('mission_media:delete'))
         <span class="fa fa-trash mission-video-item-delete" data-video="{{ $video->id }}"></span>
