28ab4944ae5cf5c88b8216dc79ab80b1730e7c71
James Kingsley <jameslkingsley@outlook.com>
2017-05-04 18:20:56 +0100

    Testing bot

--- app/Console/Commands/DiscordBot.php
+++ app/Console/Commands/DiscordBot.php
@@ -44,6 +44,7 @@ class DiscordBot extends Command
 
         $discord->on('ready', function($discord) {
             $discord->on('message', function($message, $discord) {
+                echo $message;
                 if (!str_contains($message, '@309300058669449217')) return;
 
                 $query = http_build_query([
