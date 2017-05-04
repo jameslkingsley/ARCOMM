bd294b7cd3e8f5eeba678be661ab1809520555cd
James Kingsley <jameslkingsley@outlook.com>
2017-05-04 18:11:07 +0100

    Add Discord chat bot

---
+++ app/Console/Commands/DiscordBot.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Discord\Discord;
+
+class DiscordBot extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'discord:bot';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Starts the Discord chat bot';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $discord = new Discord([
+            'token' => config('services.discord.token')
+        ]);
+
+        $discord->on('ready', function($discord) {
+            $discord->on('message', function($message, $discord) {
+                if (!str_contains($message, '@309300058669449217')) return;
+
+                $query = http_build_query([
+                    'key' => env('CLEVERBOT_API_KEY'),
+                    'input' => $message->content
+                ]);
+
+                $json = file_get_contents('https://www.cleverbot.com/getreply?'.$query);
+                $obj = json_decode($json);
+                $message->channel->sendMessage($obj->output);
+            });
+        });
+
+        $discord->run();
+    }
+}
