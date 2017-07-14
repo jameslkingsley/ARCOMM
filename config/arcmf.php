13aea5fe959272e5ac1dc93787e95f76fb425aa7
James Kingsley <jlkingsley97@gmail.com>
2017-07-14 19:30:36 +0100

    Update arcmf.php

--- config/arcmf.php
+++ config/arcmf.php
@@ -4,6 +4,7 @@ return [
     'videos' => [
         'EP 1 - Setting Up' => 'https://www.youtube.com/watch?v=rWTS-ovVhjY',
         'EP 2 - Briefing' => 'https://www.youtube.com/watch?v=otk1f5nITKg',
-        'EP 3 - Groups & Roster' => 'https://www.youtube.com/watch?v=eFsqfFiKBpM'
+        'EP 3 - Groups & Roster' => 'https://www.youtube.com/watch?v=eFsqfFiKBpM',
+        'EP 4 - Loadouts' => 'https://www.youtube.com/watch?v=6JKNNMY0QXc'
     ]
 ];
