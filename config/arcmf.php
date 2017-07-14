9ba8cbc0c1ab9727dcf3c0fd492268a30d102d6b
James Kingsley <jlkingsley97@gmail.com>
2017-07-14 18:29:55 +0100

    Update arcmf.php

--- config/arcmf.php
+++ config/arcmf.php
@@ -3,6 +3,7 @@
 return [
     'videos' => [
         'EP 1 - Setting Up' => 'https://www.youtube.com/watch?v=rWTS-ovVhjY',
-        'EP 2 - Briefing' => 'https://www.youtube.com/watch?v=otk1f5nITKg'
+        'EP 2 - Briefing' => 'https://www.youtube.com/watch?v=otk1f5nITKg',
+        'EP 3' => 'https://www.youtube.com/watch?v=eFsqfFiKBpM'
     ]
 ];
