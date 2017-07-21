4aa69580d67bc0b4f7534d644769dc807fc71f8c
James Kingsley <jameslkingsley@outlook.com>
2017-07-21 19:20:15 +0100

    Fixes

--- resources/views/join/public/confirmation.blade.php
+++ resources/views/join/public/confirmation.blade.php
@@ -7,6 +7,6 @@
 @section('content')
     <div class="content container">
         <h3>Your application has been sent</h3>
-        <p>We have sent an acknowledgment email to <strong>{{ $email }}</strong>. It can take up to 2 weeks to process.<br />You will either receive an approved or declined email.</p>
+        <p>We have sent an acknowledgment email to <strong>{{ $email }}</strong>. It can take up to 1 week to process.<br />You will either receive an approved or declined email.</p>
     </div>
 @endsection
