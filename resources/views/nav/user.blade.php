78544068c81fd0f4ab1f99015550384d783c5594
James Kingsley <jameslkingsley@outlook.com>
2018-01-03 21:53:02 +0000

    Add ARCMF download to nav

--- resources/views/nav/user.blade.php
+++ resources/views/nav/user.blade.php
@@ -1,3 +1,8 @@
+<a href="https://github.com/ARCOMM/ARCMF/releases/download/2.0.1/ARCMF.zip" class="nav-item nav-link hidden-sm-down">
+    ARCMF
+    <i class="material-icons" style="margin-top: 15px;float: right;font-size: 18px;margin-left: 5px;">file_download</i>
+</a>
+
 <li class="nav-item dropdown hidden-sm-down">
     <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="javascript:void(0)" role="button" aria-haspopup="true" aria-expanded="false">
         {{ auth()->user()->username }}
