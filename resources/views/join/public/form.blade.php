8f284c0485e0d3b98b8da0ce423cbffbf7b2f78f
James Kingsley <jameslkingsley@outlook.com>
2018-01-06 00:46:35 +0000

    Add feedback for form

--- resources/views/join/public/form.blade.php
+++ resources/views/join/public/form.blade.php
@@ -40,16 +40,26 @@
 
             <div class="row">
                 <div class="col-md-6">
-                    <div class="form-group {{ empty($errors->first('name')) ? '' : 'has-error' }}">
+                    <div class="form-group">
                         <label class="control-label">Your Name</label>
+
+                        @if ($errors->first('name'))
+                            <span class="help-block mt-2 mb-0 has-error">{{ $errors->first('name') }}</span>
+                        @endif
+
                         <span class="help-block">This is the name you use in Arma and should be the same as on TeamSpeak.</span>
                         <input type="text" name="name" class="form-control" placeholder="Name" maxlength="255" value="{{ old('name') }}" />
                     </div>
                 </div>
 
                 <div class="col-md-6">
-                    <div class="form-group {{ empty($errors->first('source_id')) ? '' : 'has-error' }}">
+                    <div class="form-group">
                         <label class="control-label">How did you find out about us?</label>
+
+                        @if ($errors->first('source_id'))
+                            <span class="help-block mt-2 mb-0 has-error">{{ $errors->first('source_id') }}</span>
+                        @endif
+
                         <span class="help-block">Knowing this helps our recruitment team.</span>
                         <select class="form-control" name="source_id" data-placeholder="Select" id="f_source_id">
                             @foreach ($sources as $source)
@@ -62,16 +72,26 @@
 
             <div class="row">
                 <div class="col-md-6">
-                    <div class="form-group {{ empty($errors->first('age')) ? '' : 'has-error' }}">
+                    <div class="form-group">
                         <label class="control-label">Your Age</label>
+
+                        @if ($errors->first('age'))
+                            <span class="help-block mt-2 mb-0 has-error">{{ $errors->first('age') }}</span>
+                        @endif
+
                         <span class="help-block">Enter your current age in numeric digits.</span>
                         <input type="number" name="age" class="form-control" placeholder="Age" value="{{ old('age') }}" />
                     </div>
                 </div>
 
                 <div class="col-md-6">
-                    <div class="form-group {{ empty($errors->first('location')) ? '' : 'has-error' }}">
+                    <div class="form-group">
                         <label class="control-label">Your Location</label>
+
+                        @if ($errors->first('location'))
+                            <span class="help-block mt-2 mb-0 has-error">{{ $errors->first('location') }}</span>
+                        @endif
+
                         <span class="help-block">You can be as specific as you like.</span>
                         <input type="text" name="location" class="form-control" placeholder="Location" value="{{ old('location') }}" />
                     </div>
@@ -80,16 +100,26 @@
 
             <div class="row">
                 <div class="col-md-6">
-                    <div class="form-group {{ empty($errors->first('email')) ? '' : 'has-error' }}">
+                    <div class="form-group">
                         <label class="control-label">Your Email Address</label>
+
+                        @if ($errors->first('email'))
+                            <span class="help-block mt-2 mb-0 has-error">{{ $errors->first('email') }}</span>
+                        @endif
+
                         <span class="help-block">We use your email address to contact you about your application. You must enter a valid and regularly checked email address.</span>
                         <input type="text" name="email" class="form-control" placeholder="Email Address" value="{{ old('email') }}" />
                     </div>
                 </div>
 
                 <div class="col-md-6">
-                    <div class="form-group {{ empty($errors->first('steam')) ? '' : 'has-error' }}">
+                    <div class="form-group">
                         <label class="control-label">Your Steam Account</label>
+
+                        @if ($errors->first('steam'))
+                            <span class="help-block mt-2 mb-0 has-error">{{ $errors->first('steam') }}</span>
+                        @endif
+
                         <span class="help-block">Please provide a link to your Steam account. You can do this by going to Steam > Your Name > Right-Click > Copy Page URL</span>
                         <input type="text" name="steam" class="form-control" placeholder="Steam Account" value="{{ old('steam') }}" />
                     </div>
@@ -98,8 +128,13 @@
 
             <div class="row">
                 <div class="col-md-4">
-                    <div class="form-group {{ empty($errors->first('available')) ? '' : 'has-error' }}">
+                    <div class="form-group">
                         <label class="control-label">Are you available Saturdays at {{ config('app.op_time', '--:--') }} time?</label>
+
+                        @if ($errors->first('available'))
+                            <span class="help-block mt-2 mb-0 has-error">{{ $errors->first('available') }}</span>
+                        @endif
+
                         <span class="help-block">This is when main operations take place.</span>
                         <select class="form-control" name="available" data-placeholder="Select">
                             <option value="0" {{ (old('available') == '0') ? 'selected="true"' : '' }}>No</option>
@@ -109,8 +144,13 @@
                 </div>
 
                 <div class="col-md-4">
-                    <div class="form-group {{ empty($errors->first('apex')) ? '' : 'has-error' }}">
+                    <div class="form-group">
                         <label class="control-label">Do you own the Apex expansion?</label>
+
+                        @if ($errors->first('apex'))
+                            <span class="help-block mt-2 mb-0 has-error">{{ $errors->first('apex') }}</span>
+                        @endif
+
                         <span class="help-block">Members are required to own the Apex expansion.</span>
                         <select class="form-control" name="apex" data-placeholder="Select">
                             <option value="0" {{ (old('apex') == '0') ? 'selected="true"' : '' }}>No</option>
@@ -120,8 +160,13 @@
                 </div>
 
                 <div class="col-md-4">
-                    <div class="form-group {{ empty($errors->first('groups')) ? '' : 'has-error' }}">
+                    <div class="form-group">
                         <label class="control-label">Are you currently in another Arma group?</label>
+
+                        @if ($errors->first('groups'))
+                            <span class="help-block mt-2 mb-0 has-error">{{ $errors->first('groups') }}</span>
+                        @endif
+
                         <span class="help-block">This won't affect your chances of acceptance.</span>
                         <select class="form-control" name="groups" data-placeholder="Select">
                             <option value="0" {{ (old('groups') == '0') ? 'selected="true"' : '' }}>No</option>
@@ -131,14 +176,24 @@
                 </div>
             </div>
 
-            <div class="form-group {{ empty($errors->first('experience')) ? '' : 'has-error' }}">
+            <div class="form-group">
                 <label class="control-label">Your Arma Experience</label>
+
+                @if ($errors->first('experience'))
+                    <span class="help-block mt-2 mb-0 has-error">{{ $errors->first('experience') }}</span>
+                @endif
+                
                 <span class="help-block">Give us a short description of your Arma experience, what mods you've used etc.</span>
                 <textarea name="experience" class="form-control" placeholder="Arma Experience" style="height:130px">{{ old('experience') }}</textarea>
             </div>
 
-            <div class="form-group {{ empty($errors->first('bio')) ? '' : 'has-error' }}">
+            <div class="form-group">
                 <label class="control-label">About Yourself</label>
+
+                @if ($errors->first('bio'))
+                    <span class="help-block mt-2 mb-0 has-error">{{ $errors->first('bio') }}</span>
+                @endif
+
                 <span class="help-block" style="margin-bottom: 0;">Tell us a bit about yourself and how you think you would contribute as a member.</span>
                 <span class="help-block" style="margin-top: 0;">
                     The more you write the better! This part is very important. Sharing a bit about yourself goes a long way. We're not looking for one-liners.
