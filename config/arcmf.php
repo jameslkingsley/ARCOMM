eba677a5432d5ab846768434e5c424a194ff5f09
James Kingsley <jameslkingsley@outlook.com>
2018-02-26 21:39:50 +0000

    Add loadout repo to list

--- config/arcmf.php
+++ config/arcmf.php
@@ -2,6 +2,7 @@
 
 return [
     'videos' => [
+        'Loadout Repo' => 'https://github.com/ARCOMM/Loadouts',
         'EP 1 - Setting Up' => 'https://www.youtube.com/watch?v=rWTS-ovVhjY',
         'EP 2 - Briefing' => 'https://www.youtube.com/watch?v=otk1f5nITKg',
         'EP 3 - Groups & Roster' => 'https://www.youtube.com/watch?v=eFsqfFiKBpM',
