5b6d0e48713449d1cdf9cf9c709643e59e4a5b4c
Jan Lédr <ledr.jan@gmail.com>
2020-06-23 17:50:18 +0200

    fixed http standard issues

--- resources/views/layout-public.blade.php
+++ resources/views/layout-public.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport">
         <meta name="viewport" content="width=device-width">
 
@@ -70,7 +70,7 @@
                         @if (auth()->guest())
                             <li>
                                 <a href="{{ url('/steamauth') }}" style="padding-top: 8px">
-                                    <img style="width: 81px" src="{{ url('/images/steam.png') }}">
+                                    <img style="width: 81px" src="{{ url('/images/steam.png') }}" alt="Steam">
                                 </a>
                             </li>
                         @endif
