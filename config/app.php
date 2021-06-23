231639c3e56095998eef34336947fe44aba79e18
Tom Burch <thomas.burch1306@gmail.com>
2021-06-23 19:04:43 +0100

    #55 - Add laravel telescope (#96)
    * Update Mission.php
    
    * Implement ValidateMissionContents
    
    * Fix + translate PBOMission errors
    
    * Remove PBOMission changes from master
    
    * Add laravel telescope
    
    * Pruning

--- config/app.php
+++ config/app.php
@@ -151,6 +151,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
         App\Providers\HelperServiceProvider::class,
 
         Spatie\MediaLibrary\MediaLibraryServiceProvider::class,
